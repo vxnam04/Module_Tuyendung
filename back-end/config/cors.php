adb0b329ea6c0392f470ac60548498f28cb88dbe
stardust-zip <nguyenhieu0802@proton.me>
2026-02-04 17:34:48 +0700

    feat: update cors

--- back-end/config/cors.php
+++ back-end/config/cors.php
@@ -3,7 +3,13 @@
 return [
     'paths' => ['*'], // cho tất cả các path
     'allowed_methods' => ['*'], // tất cả GET, POST, PUT, DELETE...
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001'], // origin frontend của bạn
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:3001'
+        'http://103.126.161.228:3001',
+        'http://103.126.161.228:3000',
+        'http://103.126.161.228:3005',
+    ], // origin frontend của bạn
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], // cho phép tất cả header
     'exposed_headers' => [],
